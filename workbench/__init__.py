8780de3917d84bffcf92b1854a24519d4dd46e8b
Zerofivem <zerofivem@gmail.com>
2026-08-30 15:19:36 +0800

    feat: export engineering v1 modules

--- workbench/__init__.py
+++ workbench/__init__.py
@@ -1,15 +1,19 @@
 from .measurement import SignalChainConfig, physical_to_adc, adc_to_physical
 from .control import BuckPlantConfig, analyze_buck_pi, pi_tustin
+from .control_advanced import PlantModel, PoleZeroController, analyze_pole_zero_loop
+from .contracts import validate_state_contract
+from .profiles import builtin_profiles, profile_from_dict
+from .protocol import Frame, crc16_ccitt, decode_frame
 from .remote import SafeMockPowerSupply
+from .sfra import parse_sfra_csv, compare_theory_to_sfra
 from .state_machine import get_state_machine
+from .validation import run_sequence
 
 __all__ = [
-    "SignalChainConfig",
-    "physical_to_adc",
-    "adc_to_physical",
-    "BuckPlantConfig",
-    "analyze_buck_pi",
-    "pi_tustin",
-    "SafeMockPowerSupply",
-    "get_state_machine",
+    "SignalChainConfig", "physical_to_adc", "adc_to_physical",
+    "BuckPlantConfig", "analyze_buck_pi", "pi_tustin",
+    "PlantModel", "PoleZeroController", "analyze_pole_zero_loop",
+    "validate_state_contract", "builtin_profiles", "profile_from_dict",
+    "Frame", "crc16_ccitt", "decode_frame", "SafeMockPowerSupply",
+    "parse_sfra_csv", "compare_theory_to_sfra", "get_state_machine", "run_sequence",
 ]
